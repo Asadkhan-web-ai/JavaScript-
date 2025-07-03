7069d76b7f9d23634e7a8ce3a9a5d4ba26616892
Asadkhan-web-ai <asadkhan11554@gmail.com>
2025-07-03 16:01:42 +0500

    Basic operations.js

---
+++ Basic operations.js
@@ -0,0 +1,11 @@
+let halfPopulation = population / 2;
+console.log(halfPopulation); // 120
+population++; // increase by 1
+console.log(population); // 241
+console.log(population > 6); // true
+let averagePopulation = 33;
+console.log(population < averagePopulation); // false
+let averagePopulation = 33;
+console.log(population < averagePopulation); // false
+let description = country + " is in " + continent + ", and its " + population + " million people speak " + language;
+console.log(description);
