bc772246b1b3700b4be6e565bf54648a71156265
Asadkhan-web-ai <asadkhan11554@gmail.com>
2025-07-03 16:05:09 +0500

    Data types.js

---
+++ Data types.js
@@ -0,0 +1,7 @@
+let isIsland = false; // Pakistan is not an island
+let language; // declare but not assigned yet
+
+console.log(typeof isIsland);   // boolean
+console.log(typeof population); // number
+console.log(typeof country);    // string
+console.log(typeof language);   // undefined
