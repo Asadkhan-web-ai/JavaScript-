8de8ab9a7a73e02a4ed11fcbe141e84523201205
Asadkhan-web-ai <asadkhan11554@gmail.com>
2025-07-03 16:07:17 +0500

    Let,cons and var.js

---
+++ Let,cons and var.js
@@ -0,0 +1,7 @@
+language = "Urdu"; // language already declared above
+const country = "Pakistan";
+const continent = "Asia";
+const isIsland = false;
+
+// Try changing one to see error
+// country = "India"; // ❌ Error: Assignment to constant variable
