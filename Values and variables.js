81b6f5b1682e30a61c021ada593445f18496a67e
Asadkhan-web-ai <asadkhan11554@gmail.com>
2025-07-03 16:06:04 +0500

    Values and variables.js

---
+++ Values and variables.js
@@ -0,0 +1,12 @@
+let country = " Pakistan";
+let continent = " Asia";
+let population = 240  // in millions
+console.log(country)
+console.log(continent)
+console.log(population)
+let island = false; // pakistan is not an island 
+let language; // declare but not assigned yet
+console.log(typeofisland);  // Booleancon
+console.log(typeofpopulation); // Number
+console. log(typesofcountry);  // String
+console. log(tyoesof);  // undefined
